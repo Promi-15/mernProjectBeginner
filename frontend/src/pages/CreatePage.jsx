420f5217b7987046fb880f20b033b71bf8ab4334
Promi-15 <promii1217@gmail.com>
2025-05-08 18:08:09 +0600

    create add product form component

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,11 +1,65 @@
-import React from 'react'
+import {
+  Box,
+  Button,
+  Container,
+  Heading,
+  Input,
+  useColorModeValue,
+  VStack,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
 
 const CreatePage = () => {
+  const [newProduct, setNewProduct] = useState({
+    name: "",
+    price: "",
+    image: "",
+  });
+  const handleAddProduct= () => {
+    console.log(newProduct)
+  }
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Container maxW={"container.sm"} size={"2xl"} textAlign={"center"} mb={8}>
+      <VStack spacing={0}>
+        <Heading as={"h1"}>create new product</Heading>
+        <Box
+          w={"full"}
+          bg={useColorModeValue("white", "gray.800")}
+          p={6}
+          rounded={"lg"}
+          shadow={"md"}
+        >
+          <VStack spacing={4}>
+            <Input
+              placeholder="Product Name"
+              name="name"
+              value={newProduct.name}
+              onChange={(e) =>
+                setNewProduct({ ...newProduct, name: e.target.value })
+              }
+            />
+            <Input
+              placeholder="Price"
+              name="price"
+              value={newProduct.price}
+              onChange={(e) =>
+                setNewProduct({ ...newProduct, price: e.target.value })
+              }
+            />
+            <Input
+              placeholder="Image Url"
+              name="image"
+              value={newProduct.image}
+              onChange={(e) =>
+                setNewProduct({ ...newProduct, image: e.target.value })
+              }
+            />
+            <Button colorScheme="blue" onClick={handleAddProduct} w={"full"}>Add Product</Button>
+          </VStack>
+        </Box>
+      </VStack>
+    </Container>
+  );
+};
 
-export default CreatePage
+export default CreatePage;
